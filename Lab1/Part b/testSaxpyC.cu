d03c92f2ba2b4d11ebc0f1c12ef0f014ca6f2646
Bryan John Baker <bake1358@umn.edu>
2021-02-19 01:08:14 -0600

    Added comments to testSaxpyC.cu code.

--- Lab1/Part b/testSaxpyC.cu
+++ Lab1/Part b/testSaxpyC.cu
@@ -3,9 +3,11 @@
 #include <sys/time.h>
 #include <unistd.h>
 
-#define NITER 100
+#define NITER 100 // The number of iterations to use kernel function.
 
 __global__ void saxpy_par(unsigned long int n, float a, float *x, float *y) {
+  //Cuda kernel function for saxpy.
+  // Result is stored back into y.
   unsigned long int i = blockIdx.x*blockDim.x + threadIdx.x;
   if (i < n)
     y[i] += a*x[i];
@@ -25,8 +27,8 @@ float saxpy_check(unsigned long int n, float a, float *x, float *y, float *z) {
     return(sqrt(s/t));
 }
 
-double wctime()
-{
+double wctime() {
+  // calculate wall time.
   struct timeval tv;
   gettimeofday(&tv, NULL);
   return (tv.tv_sec + 1E-6 * tv.tv_usec);
@@ -40,10 +42,11 @@ int main(int argc, char *argv[]) {
   double t1[NITER];
   double avgt;
   a = 1.0;
-  //a = a / (float) NITER;
+  // Allocate memory for vectors.
   x = (float*) malloc(n*sizeof(float));
   y = (float*) malloc(n*sizeof(float));
   z = (float*) malloc(n*sizeof(float));
+  // Fill vectors with random data.
   for(i=0; i < n; i++) {
     value = (float)rand() / (float)RAND_MAX;
     x[i] = value;
@@ -52,33 +55,41 @@ int main(int argc, char *argv[]) {
     value = (float)rand() / (float)RAND_MAX;
     z[i] = value;
   }
+  // Begin main for loop for testing vector length.
   for (unsigned long int vecLen = 2048; vecLen <= n; vecLen *= 2) {
+    // Allocate memory on GPU for x and y.
     cudaMalloc(&xg, vecLen*sizeof(float));
     cudaMalloc(&yg, vecLen*sizeof(float));
-    //cudaMemcpy(xg, x, vecLen*sizeof(float), cudaMemcpyHostToDevice);
-    //cudaMemcpy(yg, y, vecLen*sizeof(float), cudaMemcpyHostToDevice);
+    // Define the cuda grid and block size.
     dim3 dimGrid(1024 * 8);
     dim3 dimBlock(1024);
-    //t1 = wctime();
+    // Run saxpy kernel function NITER times on the GPU and collect time
+    // and include data transfer from CPU to GPU.  
     for(int iter=0; iter<NITER; iter++){
       t1[iter] = wctime();
+      // Copy vectors given a size of vecLen to the GPU.
       cudaMemcpy(xg, x, vecLen*sizeof(float), cudaMemcpyHostToDevice);
       cudaMemcpy(yg, y, vecLen*sizeof(float), cudaMemcpyHostToDevice);
       saxpy_par<<<dimGrid, dimBlock>>>(vecLen, a, xg, yg);
+      // Copy result from GPU to CPU.
       cudaMemcpy(z, yg, vecLen*sizeof(float), cudaMemcpyDeviceToHost);
       t1[iter] = (wctime() - t1[iter])*1.e+06;
-      //valresult = saxpy_check(vecLen, a, x, y, z);
     }
-    //t1 = (wctime() - t1)*1.e+06;
+    // Find the average time for each NITER.
     for(i=0; i<NITER; i++)
       avgt += t1[i];
     avgt /= (float) NITER;
+    // Calculate the error with check on CPU.
     valresult = saxpy_check(vecLen, a, x, y, z);
+    // Find the number of floating point ops.
     nops = (float) vecLen * 2;
+    // Print results.
     printf("** vecLen = %7.0lu, Mflops = %10.2lf  err = %2.2e\n", vecLen, nops/avgt, valresult);
+    // Free memory on GPU.
     cudaFree(xg);
     cudaFree(yg);
   }
+  // Free memory on CPU.
   free(x);
   free(y);
   free(z);

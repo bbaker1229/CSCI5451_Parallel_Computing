5249c9045e215a2c713f3d30698e3ac15189d2dc
Bryan John Baker <bake1358@umn.edu>
2021-02-23 22:16:49 -0600

    Updated Lab1 part B to save data to a file.

--- Lab1/Part b/runme.bash
+++ Lab1/Part b/runme.bash
@@ -1,8 +1,8 @@
 module load soft/cuda
 nvcc testSaxpy.cu
-echo "Running with only computations:"
-./a.out
+echo "Running with only computations:" > testSaxpy.sav
+./a.out >> testSaxpy.sav
 nvcc testSaxpyC.cu
-echo "Running with computations and data transfer:"
-./a.out
+echo "Running with computations and data transfer:" > testSaxpyC.sav
+./a.out >> testSaxpyC.sav
 rm a.out

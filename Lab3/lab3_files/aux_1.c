1cdc44ad73b2320b2a76aeff17286824009d3284
Bryan John Baker <bake1358@umn.edu>
2021-04-23 15:51:03 -0500

    Cleaned up code.

--- Lab3/lab3_files/aux_1.c
+++ Lab3/lab3_files/aux_1.c
@@ -14,11 +14,8 @@ void OneStep (PointProb** myMatrix, double** Pij, double** old_Pij,
   double *sendNS, *sendEW;
   sendNS = (double*) malloc(sizeof(double)*nj);
   sendEW = (double*) malloc(sizeof(double)*ni);
-  
-  // just copies old onto new and quits/
-  
-  //printf("My (qi qj) = (%d %d)\n",Dom->qi,Dom->qj);
 
+  // Initialize ghost vectors
   for(i=0;i<ni;i++) {
     Dom->west[i] = 0.0;
     Dom->east[i] = 0.0;
@@ -28,133 +25,35 @@ void OneStep (PointProb** myMatrix, double** Pij, double** old_Pij,
     Dom->south[j] = 0.0;
   }
 
-  /*if(Dom->qj % 2) { //if odd j rank - recv east vectors
-    for(i=0;i<ni;i++)
-      sendEW[i] = old_Pij[i][0]; // west vector, send to east vector of westNB
-    //printf("(%d %d) Sending to %d \n", Dom->qi, Dom->qj, Dom->westNB);
-    MPI_Send(sendEW, ni, MPI_DOUBLE, Dom->westNB, 0, comm2D);
-    //printf("(%d %d) Recv from %d\n", Dom->qi, Dom->qj, Dom->eastNB);
-    MPI_Recv(Dom->east, ni, MPI_DOUBLE, Dom->eastNB, 1, comm2D, &status); // recv east vector from eastNB
-  } else {
-    //printf("(%d %d) Recv from %d\n", Dom->qi, Dom->qj, Dom->eastNB);
-    MPI_Recv(Dom->east, ni, MPI_DOUBLE, Dom->eastNB, 0, comm2D, &status); // recv east vector from eastNB
-    for(i=0;i<ni;i++)
-      sendEW[i] = old_Pij[i][0]; // west vector, send to east vector of westNB
-    //printf("(%d %d) Sending to %d\n", Dom->qi, Dom->qj, Dom->westNB);
-    MPI_Send(sendEW, ni, MPI_DOUBLE, Dom->westNB, 1, comm2D);
-  }
-  //if(Dom->qi == 0 && Dom->qj == 0) printf("All East vectors completed.\n");
-
-  if(Dom->qj % 2) { //if odd j rank - recv west vectors
-    for(i=0;i<ni;i++)
-      sendEW[i] = old_Pij[i][nj-1];  // east vector, send to west vector of eastNB
-    MPI_Send(sendEW, ni, MPI_DOUBLE, Dom->eastNB, 0, comm2D);
-    MPI_Recv(Dom->west, ni, MPI_DOUBLE, Dom->westNB, 1, comm2D, &status); // recv west vector from westNB
-  } else {
-    MPI_Recv(Dom->west, ni, MPI_DOUBLE, Dom->westNB, 0, comm2D, &status); // recv west vector from westNB
-    for(i=0;i<ni;i++)
-      sendEW[i] = old_Pij[i][nj-1];  // east vector, send to west vector of eastNB
-    MPI_Send(sendEW, ni, MPI_DOUBLE, Dom->eastNB, 1, comm2D);
-  }
-  //if(Dom->qi == 0 && Dom->qj == 0) printf("All West vectors completed.\n");
-
-  if(Dom->qi % 2) { //if odd i rank - recv south vectors
-    for(j=0;j<nj;j++)
-      sendNS[j] = old_Pij[0][j]; // north vector, send to south vector of northNB
-    MPI_Send(sendNS, nj, MPI_DOUBLE, Dom->northNB, 0, comm2D);
-    MPI_Recv(Dom->south, nj, MPI_DOUBLE, Dom->southNB, 1, comm2D, &status); // recv south vector from southNB
-  } else {
-    MPI_Recv(Dom->south, nj, MPI_DOUBLE, Dom->southNB, 0, comm2D, &status); // recv south vector from southNB
-    for(j=0;j<nj;j++)
-      sendNS[j] = old_Pij[0][j]; // north vector, send to south vector of northNB
-    MPI_Send(sendNS, nj, MPI_DOUBLE, Dom->northNB, 1, comm2D);
-  }
-  //if(Dom->qi == 0 && Dom->qj == 0) printf("All South vectors completed.\n");
-
-  if(Dom->qi % 2) {  //if odd i rank - recv north vectors
-    for(j=0;j<nj;j++)
-      sendNS[j] = old_Pij[ni-1][j];  // south vector, send to north vector of southNB
-    MPI_Send(sendNS, nj, MPI_DOUBLE, Dom->southNB, 0, comm2D);
-    MPI_Recv(Dom->north, nj, MPI_DOUBLE, Dom->northNB, 1, comm2D, &status); // recv north vector from northNB
-  } else {
-    MPI_Recv(Dom->north, nj, MPI_DOUBLE, Dom->northNB, 0, comm2D, &status); // recv north vector from northNB
-    for(j=0;j<nj;j++)
-      sendNS[j] = old_Pij[ni-1][j];  // south vector, send to north vector of southNB
-    MPI_Send(sendNS, nj, MPI_DOUBLE, Dom->southNB, 1, comm2D);
-  } */
-  //if(Dom->qi == 0 && Dom->qj == 0) printf("All North vectors completed.\n");
-
-  //if(Dom->qi == 0 && Dom->qj == 0) printf("Recieved all vectors\n");
-
+  // Initialize new Pij matrix
   for(i=0; i<ni; i++)
-    for(j=0; j<nj; j++) {
-      //Pij[i][j] = old_Pij[i][j];
+    for(j=0; j<nj; j++)
       Pij[i][j] = 0.0;
-    }
 
+  // Calculate Pij entries
   for (i=0; i<ni; i++) {
     for (j=0; j<nj; j++) {
-      //Pij[i][j] = old_Pij[i][j];
-      //if(Dom->qi == 0 && Dom->qj == 0) printf("i = %d, j = %d\n", i, j);
-      if(i == 0 && j == 0) {
-        Pij[i][j]     += old_Pij[i][j] * myMatrix[i][j].prob[0];
+      Pij[i][j]       += old_Pij[i][j] * myMatrix[i][j].prob[0];
+      if(i == 0)
         Dom->north[j] += old_Pij[i][j] * myMatrix[i][j].prob[3];
-        Pij[i+1][j]   += old_Pij[i][j] * myMatrix[i][j].prob[4];
+      else
+        Pij[i-1][j]   += old_Pij[i][j] * myMatrix[i][j].prob[3];
+      if(j == 0)
         Dom->west[i]  += old_Pij[i][j] * myMatrix[i][j].prob[1];
-        Pij[i][j+1]   += old_Pij[i][j] * myMatrix[i][j].prob[2];
-      } else if(i == 0 && j == nj-1) {
-        Pij[i][j]     += old_Pij[i][j] * myMatrix[i][j].prob[0];
-        Dom->north[j] += old_Pij[i][j] * myMatrix[i][j].prob[3];
-        Pij[i+1][j]   += old_Pij[i][j] * myMatrix[i][j].prob[4];
+      else
         Pij[i][j-1]   += old_Pij[i][j] * myMatrix[i][j].prob[1];
-        Dom->east[i]  += old_Pij[i][j] * myMatrix[i][j].prob[2];
-      } else if(i == ni-1 && j == 0) {
-        Pij[i][j]     += old_Pij[i][j] * myMatrix[i][j].prob[0];
-        Pij[i-1][j]   += old_Pij[i][j] * myMatrix[i][j].prob[3];
+      if(i == ni-1)
         Dom->south[j] += old_Pij[i][j] * myMatrix[i][j].prob[4];
-        Dom->west[i]  += old_Pij[i][j] * myMatrix[i][j].prob[1];
-        Pij[i][j+1]   += old_Pij[i][j] * myMatrix[i][j].prob[2];
-      } else if(i == ni-1 && j == nj-1) {
-        Pij[i][j]     += old_Pij[i][j] * myMatrix[i][j].prob[0];
-        Pij[i-1][j]   += old_Pij[i][j] * myMatrix[i][j].prob[3];
-        Dom->south[j] += old_Pij[i][j] * myMatrix[i][j].prob[4];
-        Pij[i][j-1]   += old_Pij[i][j] * myMatrix[i][j].prob[1];
+      else 
+        Pij[i+1][j]   += old_Pij[i][j] * myMatrix[i][j].prob[4];
+      if(j == nj-1)
         Dom->east[i]  += old_Pij[i][j] * myMatrix[i][j].prob[2];
-      } else if(i == 0) {
-        Pij[i][j]     += old_Pij[i][j] * myMatrix[i][j].prob[0];
-        Dom->north[j] += old_Pij[i][j] * myMatrix[i][j].prob[3];
-        Pij[i+1][j]   += old_Pij[i][j] * myMatrix[i][j].prob[4];
-        Pij[i][j-1]   += old_Pij[i][j] * myMatrix[i][j].prob[1];
+      else
         Pij[i][j+1]   += old_Pij[i][j] * myMatrix[i][j].prob[2];
-      } else if(i == ni-1) {
-        Pij[i][j]     += old_Pij[i][j] * myMatrix[i][j].prob[0];
-        Pij[i-1][j]   += old_Pij[i][j] * myMatrix[i][j].prob[3];
-        Dom->south[j] += old_Pij[i][j] * myMatrix[i][j].prob[4];
-        Pij[i][j-1]   += old_Pij[i][j] * myMatrix[i][j].prob[1];
-        Pij[i][j+1]   += old_Pij[i][j] * myMatrix[i][j].prob[2];
-      } else if(j == 0) {
-        Pij[i][j]     += old_Pij[i][j] * myMatrix[i][j].prob[0];
-        Pij[i-1][j]   += old_Pij[i][j] * myMatrix[i][j].prob[3];
-        Pij[i+1][j]   += old_Pij[i][j] * myMatrix[i][j].prob[4];
-        Dom->west[i]  += old_Pij[i][j] * myMatrix[i][j].prob[1];
-        Pij[i][j+1]   += old_Pij[i][j] * myMatrix[i][j].prob[2];
-      } else if(j == nj-1) {
-        Pij[i][j]     += old_Pij[i][j] * myMatrix[i][j].prob[0];
-        Pij[i-1][j]   += old_Pij[i][j] * myMatrix[i][j].prob[3];
-        Pij[i+1][j]   += old_Pij[i][j] * myMatrix[i][j].prob[4];
-        Pij[i][j-1]   += old_Pij[i][j] * myMatrix[i][j].prob[1];
-        Dom->east[i]  += old_Pij[i][j] * myMatrix[i][j].prob[2];
-      } else {
-        Pij[i][j]     += old_Pij[i][j] * myMatrix[i][j].prob[0];
-        Pij[i-1][j]   += old_Pij[i][j] * myMatrix[i][j].prob[3];
-        Pij[i+1][j]   += old_Pij[i][j] * myMatrix[i][j].prob[4];
-        Pij[i][j-1]   += old_Pij[i][j] * myMatrix[i][j].prob[1];
-        Pij[i][j+1]   += old_Pij[i][j] * myMatrix[i][j].prob[2];
-      }
     }
   }
 
-  //Send the ghost vectors back and add them to their new vectors  ----- Fix this part......
+  //Send the ghost vectors back and add them to their new vectors
   if(Dom->qj % 2) { //if odd j rank - recv east vectors
     MPI_Send(Dom->west, ni, MPI_DOUBLE, Dom->westNB, 0, comm2D);
     MPI_Recv(sendEW, ni, MPI_DOUBLE, Dom->eastNB, 1, comm2D, &status);
@@ -166,7 +65,6 @@ void OneStep (PointProb** myMatrix, double** Pij, double** old_Pij,
       Pij[i][nj-1] += sendEW[i];
     MPI_Send(Dom->west, ni, MPI_DOUBLE, Dom->westNB, 1, comm2D);
   }
-  //if(Dom->qi == 0 && Dom->qj == 0) printf("All East vectors completed.\n");
 
   if(Dom->qj % 2) { //if odd j rank - recv west vectors
     MPI_Send(Dom->east, ni, MPI_DOUBLE, Dom->eastNB, 0, comm2D);
@@ -179,7 +77,6 @@ void OneStep (PointProb** myMatrix, double** Pij, double** old_Pij,
       Pij[i][0] += sendEW[i];
     MPI_Send(Dom->east, ni, MPI_DOUBLE, Dom->eastNB, 1, comm2D);
   }
-  //if(Dom->qi == 0 && Dom->qj == 0) printf("All West vectors completed.\n");
 
   if(Dom->qi % 2) { //if odd i rank - recv south vectors
     MPI_Send(Dom->north, nj, MPI_DOUBLE, Dom->northNB, 0, comm2D);
@@ -192,7 +89,6 @@ void OneStep (PointProb** myMatrix, double** Pij, double** old_Pij,
       Pij[ni-1][j] += sendNS[j];
     MPI_Send(Dom->north, nj, MPI_DOUBLE, Dom->northNB, 1, comm2D);
   }
-  //if(Dom->qi == 0 && Dom->qj == 0) printf("All South vectors completed.\n");
 
   if(Dom->qi % 2) { //if odd i rank - recv north vectors
     MPI_Send(Dom->south, nj, MPI_DOUBLE, Dom->southNB, 0, comm2D);
@@ -205,8 +101,6 @@ void OneStep (PointProb** myMatrix, double** Pij, double** old_Pij,
       Pij[0][j] += sendNS[j];
     MPI_Send(Dom->south, nj, MPI_DOUBLE, Dom->southNB, 1, comm2D);
   }
-  //if(Dom->qi == 0 && Dom->qj == 0) printf("All North vectors completed.\n");
-
 }
 
 
